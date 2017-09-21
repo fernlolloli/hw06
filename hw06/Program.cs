e7b081ba7243aae252f2d035c1e0aa5396351565
napatsorn kamsaard <59030174@kmitl.ac.th>
2017-09-21 21:29:40 +0700

    test1

--- hw06/Program.cs
+++ hw06/Program.cs
@@ -8,8 +8,21 @@ namespace hw06
 {
     class Program
     {
-        static void Main(string[] args)
+        static void Main()
         {
+            int val;
+            Console.WriteLine("in put a number you want to check evven of odd");
+            val = int.Parse(Console.ReadLine());
+            check(ref val);
+            Console.WriteLine(val);
+            Console.ReadLine();
+        }
+        static void check(ref int i)
+        {
+            if (i % 2 == 0)
+            {
+                Console.WriteLine("even");
+            }
         }
     }
 }
